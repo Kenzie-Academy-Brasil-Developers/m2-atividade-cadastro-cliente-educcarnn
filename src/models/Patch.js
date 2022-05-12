b298a36bb529a4f9259bff7985bb887fd5d51377
Eduardo Carneiro <81482133+educcarnn@users.noreply.github.com>
2022-05-12 17:48:12 -0300

    Last alterations method patch

--- src/models/Patch.js
+++ src/models/Patch.js
@@ -2,20 +2,56 @@ import { Api } from "../js/Api.js";
 
 const clientes = await Api.listarClientes()
 
-
-
 class Patch {
-    static metodo(elem) {
-        elem.forEach(({nome}) => {
+    /*
+    static user(elem) {
+        elem.forEach(({nome, id}) => {
             const select = document.querySelector('#buscarCliente')
         
             const option = document.createElement('option')
             option.innerText = nome
 
+            option.value = id
             select.appendChild(option)
         })
     }
+*/
+    static AllDates(){
+         
+        clientes.forEach(({nome, id}) => {
+            const select = document.querySelector('#buscarCliente')
+        
+            const option = document.createElement('option')
+            option.innerText = nome
+
+            option.value = id
+            select.appendChild(option)
+        })
+
+    }
+
+    static preencher(){
+        clientes.forEach(() => {
+
+        })
+    }
 }
-Patch.metodo(clientes)
+
+
+
+Patch.AllDates()
+/*
+const inputBtn = document.querySelector('#inputCliente')
+inputBtn.addEventListener('click', Patch.AllDates)
+*/
+
+
+/*
+Adicionar Event no button,
+
+para que percorra cada dados, e preencha os valores automaticamente 
+
+No botão de aditar, percorrer se todos os dados são iguais, caso não seja, fazer a modificação
+*/
 Api.editarCliente()
 
