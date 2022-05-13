085797f172416605ced793ff556e11904c4d6ecd
Eduardo Carneiro <81482133+educcarnn@users.noreply.github.com>
2022-05-13 01:12:18 -0300

    Editing content with patch method api

--- src/models/Patch.js
+++ src/models/Patch.js
@@ -2,48 +2,46 @@ import { Api } from "../js/Api.js";
 
 const clientes = await Api.listarClientes()
 
+const validation = document.querySelector('#validation')
+const firstForm = document.querySelector('#firstDados')
+const genero = document.querySelector('#sexo')
+const selectSpread = [...firstForm]
+
+const secondForm = document.querySelector('#secondDados')
+const secondSpread = [...secondForm]
+
 const telSpread = document.querySelector('#telForm')
 const tel = [...telSpread]
 
 class Patch {
-    /*
-    static user(elem) {
-        elem.forEach(({nome, id}) => {
-            const select = document.querySelector('#buscarCliente')
+static Verify() {
+     clientes.forEach(({id, nome, data_nasc, sexo, email, endereco, telefone_fixo}) => {
+        const { masculino, feminino } = sexo
+        const { cep, bairro, cidade, estado, numero, rua } = endereco
+
+          if(id === Number(validation.value)) {
+                if(nome !== selectSpread[0].value) {
+                    Api.editarCliente(id, {nome: selectSpread[0].value})
+                } 
+                if(data_nasc !== selectSpread[1].value) {
+                    Api.editarCliente(id, {data_nasc: selectSpread[1].value})
+                }
+                if(sexo !== genero.value) {
+                    Api.editarCliente(id, {sexo: genero.value})
+                }
+                if(email !== selectSpread[3].value) {
+                    Api.editarCliente(id, {email: selectSpread[3].value})
+                }
+                
+          }
         
-            const option = document.createElement('option')
-            option.innerText = nome
+    })
 
-            option.value = id
-            select.appendChild(option)
-        })
-    }
-*/
-    
-    static Verify() {
-        let obj = {}
-
-     clientes.forEach(({id}) =>{
-        console.log(clientes.id)
-     
-     })  
-    }
+}
 }
 
 const btn = document.querySelector('#edit')
 btn.addEventListener('click', Patch.Verify)
-/*
-const inputBtn = document.querySelector('#inputCliente')
-inputBtn.addEventListener('click', Patch.AllDates)
-*/
 
-
-/*
-Adicionar Event no button,
-
-para que percorra cada dados, e preencha os valores automaticamente 
-
-No botão de aditar, percorrer se todos os dados são iguais, caso não seja, fazer a modificação
-*/
 
 
--- src/models/Popular.js
+++ src/models/Popular.js
@@ -1,51 +1,61 @@
 import { Api } from "../js/Api.js";
 const clientes = await Api.listarClientes()
 
+const validation = document.querySelector('#validation')
+const firstForm = document.querySelector('#firstDados')
+const genero = document.querySelector('#sexo')
+const selectSpread = [...firstForm]
+
+const secondForm = document.querySelector('#secondDados')
+const secondSpread = [...secondForm]
+
+const telSpread = document.querySelector('#telForm')
+const tel = [...telSpread]
+
 class Popular {
     static AllDates() {
-        clientes.forEach(({ nome }) => {
+        clientes.forEach(({ nome, id}) => {
             const select = document.querySelector('#buscarCliente')
-
+    
+    
             const option = document.createElement('option')
             option.innerText = nome
-
+         
+    
             select.appendChild(option)
         })
     }
-
+    
     static Popular() {
-        const firstForm = document.querySelector('#firstDados')
-        const genero = document.querySelector('#sexo')
-        const selectSpread = [...firstForm]
-        const secondForm = document.querySelector('#secondDados')
-        const secondSpread = [...secondForm]
-
-        clientes.forEach(({ nome, data_nasc, sexo, email, endereco, telefone_fixo }) => {
+        clientes.forEach(({id,nome, data_nasc, sexo, email, endereco, telefone_fixo }) => {
             const { masculino, feminino } = sexo
             const { cep, bairro, cidade, estado, numero, rua } = endereco
-
-
+    
             const selector = document.querySelector('#buscarCliente').value
             if (selector === nome) {
+                validation.innerHTML = ''
+                validation.value = id
+    
                 selectSpread[0].value = nome
+                //selectSpread[0].id = id
                 selectSpread[1].value = data_nasc
                 genero.value = sexo
                 selectSpread[3].value = email
-
+    
                 secondSpread[0].value = cep
                 secondSpread[1].value = rua
                 secondSpread[2].value = numero
                 secondSpread[3].value = bairro
                 secondSpread[4].value = cidade
                 secondSpread[5].value = estado
-
+    
                 tel[0].value = telefone_fixo
             }
         })
     }
 }
 
+Popular.AllDates()
+
 const value = document.querySelector('#inputCliente')
 value.addEventListener('click', Popular.Popular)
-
-Popular.AllDates()
